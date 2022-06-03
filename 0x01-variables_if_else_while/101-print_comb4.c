1354b87787553d7574fed354b1f4769896e15fa8
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-06-03 16:55:50 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,61 +1,31 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- *
- * * main-print all possible combinations of two two digits
- *
- * * Return: 0 if successful
- *
- * */
-
+* main-print all possible combinations of two two digits
+* Return: 0 if successful
+*/
 int main(void)
-
 {
+int i, j;
 
-	int i, j;
-
-
-
-	for (i = 0; i < 100; i++)
-
-	{
-
-		for (j = 0; j < 100; j++)
-
-		{
-
-			if (i < j)
-
-			{
-
-				putchar((i / 10) + 48);
-
-				putchar((i % 10) + 48);
-
-				putchar(' ');
-
-				putchar((j / 10) + 48);
-
-				putchar((j % 10) + 48);
-
-				if (i != 98 || j != 99)
-
-				{
-
-					putchar(',');
-
-					putchar(' ');
-
-				}
-
-			}
-
-		}
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
