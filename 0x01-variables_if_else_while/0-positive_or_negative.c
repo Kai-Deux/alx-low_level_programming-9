fe302bc0e0cfbe8bbce0dfdc48d619b3e3855fa7
E-Viviane <vewedje@gmail.com>
2022-06-03 07:07:32 -0700

    My variables commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,38 +8,54 @@
 
 /**
  *
- * * main-assigns a random number to int n each time
+ *  * main - Entry point
  *
- * * it is executed, and prints it
+ *   *
  *
- * * Return: 0 if successful
+ *    * Return: Always return 0
  *
- * */
+ *     */
+
+
 
 int main(void)
 
 {
 
-	int n;
+		int n;
 
 
 
-	srand(time(0));
+			srand(time(0));
 
-	n = rand() - RAND_MAX / 2;
+				n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
 
-		printf("%d is positive\n", n);
 
-	else if (n == 0)
+					if (n < 0)
 
-		printf("%d is zero\n", n);
+							{
 
-	else if (n < 0)
+										printf("%d is negative\n", n);
 
-		printf("%d is negative\n", n);
+											}
 
-	return (0);
+						else if (n > 0)
+
+								{
+
+											printf("%d is positive\n", n);
+
+												}
+
+							else
+
+									{
+
+												printf("%d is zero\n", n);
+
+													}
+
+								return (0);
 
 }
