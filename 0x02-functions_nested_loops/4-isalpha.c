c273d7572ccb2247ac9a59a1143c5273ca8dc9b8
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-06-07 14:00:11 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,39 +1,20 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
  *
- *  * _islower - checks for lowercase
- *
- *   * @c: c is an ascii character
- *
- *    *
- *
- *     * Return: 1 if lowercase
- *
- *      */
+ * Return: 1 (if letter)
+ */
 
-
-
-int _islower(int c)
-
+int _isalpha(int c)
 {
-
-		if (c >= 'a' && c <= 'z')
-
-				{
-
-							return (1);
-
-								}
-
-			else
-
-					{
-
-								return (0);
-
-									}
-
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
