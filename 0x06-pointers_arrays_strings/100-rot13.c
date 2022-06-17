4d5fb4f3bc23150d2c9ccb59843c5dcbcdb2b26a
E-Viviane <vewedje@gmail.com>
2022-06-17 11:34:55 -0700

    My programming commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,55 @@
 #include "main.h"
+
 /**
- *rot13 - encoding a string using rot13.
- *@s: the string to be encoded to rot13.
- *Return: the string s encoded to rot13.
- */
+ *
+ *  * rot13 - encodes a string using rot13
+ *
+ *   * @s: input string.
+ *
+ *    * Return: the pointer to dest.
+ *
+ *     */
+
+
 
 char *rot13(char *s)
+
 {
-int i = 0, j = 0;
 
-char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+		int count = 0, i;
 
-char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+			char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-while (s[i] != '\0')
-{
-do{
-if (s[i] == string_alpha[j])
-{
-s[i] = string_rot13[j];
-break;
-}
-j++;
-}
-while (string_alpha[j] != '\0');
-}
-j = 0;
-i++;
+				char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-return (s);
+
+
+					while (*(s + count) != '\0')
+
+							{
+
+										for (i = 0; i < 52; i++)
+
+													{
+
+																	if (*(s + count) == alphabet[i])
+
+																					{
+
+																										*(s + count) = rot13[i];
+
+																														break;
+
+																																	}
+
+																			}
+
+												count++;
+
+													}
+
+
+
+						return (s);
+
 }
